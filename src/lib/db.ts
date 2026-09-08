d9e03662191db6e2a02b1907b10d554217eb16c2
dipishBisht <dipish1718@gmail.com>
2026-09-09 03:40:45 +0530

    fx: buid error fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,20 +1,38 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error("Please define  MONGODB_URI in .env.local");
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI in your environment variables");
+}
 
-let cached = global.mongoose;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
 
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongoose ?? {
+  conn: null,
+  promise: null,
+};
+
+global.mongoose = cached;
 
 async function dbConnect() {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-  if (!cached.promise)
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!);
+  }
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 }
 
